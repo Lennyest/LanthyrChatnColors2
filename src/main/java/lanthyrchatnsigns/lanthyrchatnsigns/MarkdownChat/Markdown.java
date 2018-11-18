aeb2ebfb25ab6706f5aa5f751127244dd8fea876
GreatThane <ep9630@gmail.com>
2018-11-18 03:38:04 -0600

    Allow for both vanilla and Herochat functionality

--- src/main/java/lanthyrchatnsigns/lanthyrchatnsigns/MarkdownChat/Markdown.java
+++ src/main/java/lanthyrchatnsigns/lanthyrchatnsigns/MarkdownChat/Markdown.java
@@ -1,28 +1,31 @@
 package lanthyrchatnsigns.lanthyrchatnsigns.MarkdownChat;
 
+import com.dthielke.herochat.ChannelChatEvent;
+import com.dthielke.herochat.Chatter;
+import com.dthielke.herochat.Herochat;
+import com.dthielke.herochat.MessageHandler;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Markdown implements Listener {
 
     private static final Pattern ASTERISK_PATTERN = Pattern.compile("(\\*+|_)(.*?)(\\*+|_)", Pattern.MULTILINE | Pattern.CASE_INSENSITIVE);
 
-    public static BaseComponent[] methodUsingPattern(String testString, String format, String username) {
+    public static BaseComponent[] methodUsingPattern(String testString, String[] uffixes) {
 
         Matcher matcher = ASTERISK_PATTERN.matcher(testString);
-        format = format.replaceAll("%1\\$s", username);
-        String[] uffixes = format.split("%2\\$s");
         ComponentBuilder builder = new ComponentBuilder(uffixes[0]);
         int lastEnd = 0;
 
@@ -50,7 +53,6 @@ public class Markdown implements Listener {
                 builder.underlined(true);
             }
             lastEnd = matcher.end();
-
         }
         builder.append(testString.substring(lastEnd), ComponentBuilder.FormatRetention.NONE);
         if (uffixes.length > 1) builder.append(uffixes[1], ComponentBuilder.FormatRetention.NONE);
@@ -59,11 +61,5 @@ public class Markdown implements Listener {
         return builder.create();
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public static void boldMarkDown(AsyncPlayerChatEvent event) {
-        for (Player player : event.getRecipients()) {
-            player.spigot().sendMessage(ChatMessageType.CHAT, methodUsingPattern(event.getMessage(), event.getFormat(), event.getPlayer().getDisplayName()));
-        }
-        event.setCancelled(true);
-    }
+    private static final Pattern COLOR_CODE_PATTERN = Pattern.compile("(?i)(&)([0-9a-fk-or])");
 }
